05eed22c72a4ac0b05f59f0256cee28db126325c
miguel-gerena <miguel.a.gerena@gmail.com>
2024-07-04 15:59:48 -0400

    merge function has to update locations of every element of the location dict.  Abandoned in favor of simple 2 case loop.

--- cs336_basics/BPE_tokenizer.py
+++ cs336_basics/BPE_tokenizer.py
@@ -1,13 +1,13 @@
 import regex as re
 from dataclasses import dataclass
-from typing import List, Tuple, Dict, DefaultDict
+from typing import List, Tuple, Dict, DefaultDict, OrderedDict as OD
 from collections import defaultdict, OrderedDict
 import heapq
 
 def get_regex_pattern() -> str: 
     return r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
-def pre_tokenize(pattern:str, filename:str, endLine:bool=False) -> str:
+def pre_tokenize(pattern:str, filename:str, endLine:bool=True) -> str:
     with open(filename, "r") as F:
         if not endLine:
             pre_tokenized_output:str = "".join(re.findall(pattern, "".join(line for line in F.read().splitlines())))
@@ -21,9 +21,9 @@ class BPETokenizerParams:
     vocab: Dict[int, bytes]            
     merges: Dict[Tuple[int, int], int]  
 
-def merge(indices: List[int], pair: Tuple[(int, int)], new_index: int, locations:Dict[int, list]) -> Tuple[List[int], Dict[int, int]]:
+def merge(indices: List[int], pair: Tuple[(int, int)], new_index: int, locations:OD[int, list], isdebug:bool =True) -> Tuple[List[int], OD[int, list]]:
     """Return `indices`, but with all instances of `pair` replaced with `new_index`."""
-    new_indices: list = []
+    new_indices: List[int] = []
     last_index: int = 0
     locations[new_index] = []  #ordered dict intializing new index
     locations_list: list = locations.pop(pair[0])
@@ -36,25 +36,34 @@ def merge(indices: List[int], pair: Tuple[(int, int)], new_index: int, locations
             locations[new_index].append(len(new_indices) - 1)
 
             #handle the location of the second item in the pair
-            locations[pair[1]].remove(location + 1)
-            if not locations[pair[1]]:
+            if locations[pair[1]]:
+                locations[pair[1]].remove(location + 1)
+            else:
                 locations.pop(pair[1])
+            last_index = location + 2  # found a pair
         else:
-            new_locations.append(location)
-        last_index = location + 2
+            new_indices.append(indices[location])
+            new_locations.append(len(new_indices) - 1)
+            last_index = location + 1  # not a pair check next
+
     new_indices.extend(indices[last_index:])
 
     if new_locations: 
+        if isdebug:
+            for location in new_locations:
+                assert pair[0] == new_indices[location]
         locations[pair[0]] = new_locations
     
     return new_indices, locations
 
 def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple[Dict[int, bytes], List[Tuple[bytes, bytes]]]:
         pre_tokenized = pre_tokenize(get_regex_pattern(), input_path)
-        # with open(input_path, "r") as F:
-        #     pre_tokenized = F.read().encode("utf-8")
-        pre_tokenized = pre_tokenized.encode("utf-8")
-        location_indices = OrderedDict()  #Ordered so we can use it as LRU cache when we hit vocab limit
+        # with open(input_path, "r") as text:
+            # pre_tokenized = F.read().encode("utf-8")
+        pre_tokenized_bytes:bytes = pre_tokenized.encode("utf-8")
+        del pre_tokenized
+
+        location_indices: OD = OrderedDict()  #Ordered so we can use it as LRU cache when we hit vocab limit
         indices: list = []
         merges: List[Tuple[bytes, bytes]] = []
         vocab: Dict[int, bytes] = {x:bytes([x]) for x in range(256)}
@@ -62,8 +71,8 @@ def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple
         for i in range(len(special_tokens)):
             vocab[256 + i] = bytes(special_tokens[i].encode("utf-8"))
 
-        for i in range(len(pre_tokenized)):
-            index = int(pre_tokenized[i])
+        for i in range(len(pre_tokenized_bytes)):
+            index = int(pre_tokenized_bytes[i])
             indices.append(index)
             not_new_slice: list = location_indices.get(index, [])
             if not_new_slice:
@@ -73,7 +82,7 @@ def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple
                 location_indices[index] = not_new_slice
 
         start_index = len(vocab)
-        num_merges = 244
+        num_merges = 3
         for i in range(num_merges):
             counts: DefaultDict[Tuple[int, int], int] = defaultdict(int)
 
@@ -94,23 +103,24 @@ def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple
             #     vocab.pop(location_indices.popitem(last=False)[0]) # LRU removal scheme
         
         return vocab, merges
-class BPETokenizer():
-    def __init__(self, input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple[Dict[int, bytes], List[Tuple[bytes, bytes]]]:
-        pass
+# class BPETokenizer():
+#     def __init__(self, input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple[Dict[int, bytes], List[Tuple[bytes, bytes]]]:
+#         pass
 
-    def encode(self, text: str) -> List[int]:
-        pre_tokenized = pre_tokenize(get_regex_pattern(), self.input_path)
-        # Note: this is a very slow implementation
-        for pair, new_index in self.params.merges.items():
-            indices = merge(indices, pair, new_index)
-        return indices
+#     def encode(self, text: str) -> List[int]:
+#         pre_tokenized = pre_tokenize(get_regex_pattern(), self.input_path)
+#         # Note: this is a very slow implementation
+#         for pair, new_index in self.params.merges.items():
+#             indices = merge(indices, pair, new_index)
+#         return indices
 
-    def decode(self, indices: List[int]) -> str:
-        bytes_list = list(map(self.vocab.get, indices))
-        text = b"".join(bytes_list).decode("utf-8")
-        return text
+#     def decode(self, indices: List[int]) -> str:
+#         bytes_list = list(map(self.vocab.get, indices))
+#         text = b"".join(bytes_list).decode("utf-8")
+#         return text
 
 
-pattern = get_regex_pattern()
-print(pre_tokenize(pattern, "test.txt"))
+# pattern = get_regex_pattern()
+# print(pre_tokenize(pattern, "test.txt"))
+train_BPE("test.txt", 1000, [])
 
