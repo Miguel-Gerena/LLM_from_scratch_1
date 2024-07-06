2229905504578cd4db973b415d292a9210fb6e36
miguel-gerena <miguel.a.gerena@gmail.com>
2024-07-06 09:03:40 -0400

    added priority queue and simplified merges.  Debugging.  Only doing 1 count need to fix.

--- cs336_basics/BPE_tokenizer.py
+++ cs336_basics/BPE_tokenizer.py
@@ -1,19 +1,20 @@
 import regex as re
 from dataclasses import dataclass
-from typing import List, Tuple, Dict, DefaultDict, OrderedDict as OD
+from typing import List,Tuple, Dict, DefaultDict, OrderedDict as OD
 from collections import defaultdict, OrderedDict
 import heapq
+import json
 
 def get_regex_pattern() -> str: 
-    return r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    return re.compile(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
 
-def pre_tokenize(pattern:str, filename:str, endLine:bool=True) -> str:
+def pre_tokenize(pattern:str, filename:str, endLine:bool=False) -> str:
     with open(filename, "r") as F:
         if not endLine:
-            pre_tokenized_output:str = "".join(re.findall(pattern, "".join(line for line in F.read().splitlines())))
+            pre_tokenized_output:str = "".join(re.findall(pattern, "".join(line + " " for line in F.read().splitlines())))
         else:
             pre_tokenized_output:str = "".join(re.findall(pattern, F.read()))
-    return pre_tokenized_output
+    return pre_tokenized_output.strip()
 
 
 @dataclass(frozen=True)
@@ -21,55 +22,34 @@ class BPETokenizerParams:
     vocab: Dict[int, bytes]            
     merges: Dict[Tuple[int, int], int]  
 
-def merge(indices: List[int], pair: Tuple[(int, int)], new_index: int, locations:OD[int, list], isdebug:bool =True) -> Tuple[List[int], OD[int, list]]:
-    """Return `indices`, but with all instances of `pair` replaced with `new_index`."""
+def merge(indices: List[int], pair: Tuple[(int, int)], new_index: int) -> List[int]:
     new_indices: List[int] = []
-    last_index: int = 0
-    locations[new_index] = []  #ordered dict intializing new index
-    locations_list: list = locations.pop(pair[0])
-    new_locations: list = []
-
-    for location in locations_list:
-        new_indices.extend(indices[last_index:location])
-        if location + 1 < len(indices) and (indices[location], indices[location + 1]) == pair:
+    i: int = 0
+    while i < len(indices):
+        if i + 1 < len(indices) and (indices[i],indices[i+1]) == pair:
             new_indices.append(new_index)
-            locations[new_index].append(len(new_indices) - 1)
-
-            #handle the location of the second item in the pair
-            if locations[pair[1]]:
-                locations[pair[1]].remove(location + 1)
-            else:
-                locations.pop(pair[1])
-            last_index = location + 2  # found a pair
+            i += 2
         else:
-            new_indices.append(indices[location])
-            new_locations.append(len(new_indices) - 1)
-            last_index = location + 1  # not a pair check next
+            new_indices.append(indices[i])
+            i += 1
+    return new_indices
 
-    new_indices.extend(indices[last_index:])
-
-    if new_locations: 
-        if isdebug:
-            for location in new_locations:
-                assert pair[0] == new_indices[location]
-        locations[pair[0]] = new_locations
-    
-    return new_indices, locations
 
 def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple[Dict[int, bytes], List[Tuple[bytes, bytes]]]:
-        pre_tokenized = pre_tokenize(get_regex_pattern(), input_path)
+        regex:re.Pattern = get_regex_pattern()
+        pre_tokenized = pre_tokenize(regex, input_path)
+        print(pre_tokenized)
         # with open(input_path, "r") as text:
             # pre_tokenized = F.read().encode("utf-8")
         pre_tokenized_bytes:bytes = pre_tokenized.encode("utf-8")
         del pre_tokenized
+        vocab: Dict[int, bytes]  = {i:bytes(special_tokens[i].encode("utf-8")) for i in range(len(special_tokens))}
 
         location_indices: OD = OrderedDict()  #Ordered so we can use it as LRU cache when we hit vocab limit
         indices: list = []
         merges: List[Tuple[bytes, bytes]] = []
-        vocab: Dict[int, bytes] = {x:bytes([x]) for x in range(256)}
-        
-        for i in range(len(special_tokens)):
-            vocab[256 + i] = bytes(special_tokens[i].encode("utf-8"))
+        for x in range(256):
+            vocab[x + len(special_tokens)] = bytes([x])
 
         for i in range(len(pre_tokenized_bytes)):
             index = int(pre_tokenized_bytes[i])
@@ -82,25 +62,63 @@ def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple
                 location_indices[index] = not_new_slice
 
         start_index = len(vocab)
-        num_merges = 3
-        for i in range(num_merges):
-            counts: DefaultDict[Tuple[int, int], int] = defaultdict(int)
+        counts: DefaultDict[Tuple[int, int], int] = defaultdict(int)
+        heap: List[Tuple[int, Tuple[int, int]]] = []
+        for pair in zip(indices, indices[1:]):
+            counts[pair] += 1
+        
+        for key, value in counts.items():
+            heapq.heappush(heap, (-value, key))
+        
 
-            for pair in zip(indices, indices[1:]):
-                counts[pair] += 1
-
+        curr_index = 0
+        while heap and len(vocab) < vocab_size:
             #most common pair maybe need to add ordering logic in case of ties
-            if not counts:
-                break
-            pair = max(counts, key=counts.get) 
-            new_index = start_index + i
+            pair: Tuple[int, int]  = (0, 0)
+            max_counts: int = 0
+
+            max_counts, pair = heapq.heappop(heap)
+
+            ties: List[Tuple[str, Tuple[int, int]]] = [("".join([vocab[x].decode("utf-8") for x in pair]), pair)]
+            while heap[0][0] == max_counts:
+                next_pair: Tuple[int, int] = heapq.heappop(heap)[1]
+                ties.append(("".join([vocab[x].decode("utf-8") for x in next_pair]), next_pair))
+
+            sorted_ties:List[Tuple[str, Tuple[int, int]]] = sorted(ties, reverse=True)
+            for i in range(len(sorted_ties)):     
+                if i == 0:
+                    pair = sorted_ties[i][1]
+                else:
+                    heapq.heappush(heap, (max_counts, sorted_ties[i][1]))
+
+            new_index = start_index + curr_index
+            curr_index += 1
             merges.append((vocab[pair[0]], vocab[pair[1]]))
             vocab[new_index] = vocab[pair[0]] + vocab[pair[1]]
 
-            indices, location_indices = merge(indices, pair, new_index, location_indices)
+            indices = merge(indices, pair, new_index)
+
 
             # if len(vocab) > vocab_size:
             #     vocab.pop(location_indices.popitem(last=False)[0]) # LRU removal scheme
+        # with open("merges.txt", "w") as F:
+        #     for merge_item in merges:
+        #         F.write(f"{merge_item[0]} {merge_item[1]}\n" )
+        # jsondump = {}
+        # for key, value in vocab.items():
+        #     jsondump[value.decode("utf-8")] = key
+
+        
+        # with open("vocab.json", "w") as F:
+        # #     json.dump(jsondump, F)
+        # with open("my_merges2.txt", "w") as f:
+        #     for key, val in merges:
+        #         f.write(f"({key} {val})\n")
+        
+        # with open("my_vocab2.txt", "w") as f:
+        #     for key, val in vocab.items():
+        #         f.write(f"({key}:{val})\n")
+
         
         return vocab, merges
 # class BPETokenizer():
@@ -122,5 +140,6 @@ def train_BPE(input_path:str, vocab_size:int, special_tokens:List[str]) -> Tuple
 
 # pattern = get_regex_pattern()
 # print(pre_tokenize(pattern, "test.txt"))
-train_BPE("test.txt", 1000, [])
+
+vocab, merges = train_BPE("tests/fixtures/corpus.en", 500, ['<|endoftext|>'])
 
