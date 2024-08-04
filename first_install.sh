2258e3b440c3314f4e0e47f28c850cf118412a1e
miguel-gerena <miguel.a.gerena@gmail.com>
2024-08-04 18:21:02 -0400

    heads

--- first_install.sh
+++ first_install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install git-lfs -y
 git-lfs install
 
-pip install -r requirements.txt
-pip install -e -.
-
 git clone https://github.com/Miguel-Gerena/LLM_from_scratch_1.git
+
+pip install -r LLM_from_scratch_1/requirements.txt
+pip install -e LLM_from_scratch_1/
